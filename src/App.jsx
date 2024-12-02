36da996c0c5e7af7b9c00275dd4650d1682db4ab
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-12-02 16:23:12 +0100

    ADD form to create new post by adding a post name

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import './App.css';
 import Card from './components/Card/Card';
 import Footer from './components/Footer/Footer';
 import posts from './data/posts';
+import { useState } from 'react';
 
 function App() {
+    const [drawnPosts, setPosts] = useState(posts);
+    const [postName, setPostName] = useState('Titolo');
+
     const tags = [];
-    posts.forEach((post) => {
+    drawnPosts.forEach((post) => {
         post.tags.forEach((tag) => {
             !tags.includes(tag) && tags.push(tag);
         });
     });
 
-    console.log(tags);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newPost = {
+            id: posts.length + 1,
+            title: postName,
+            published: true,
+            tags: [],
+            image: '',
+            content: '',
+        };
+
+        setPosts([...drawnPosts, newPost]);
+    };
 
     return (
         <>
@@ -20,13 +36,25 @@ function App() {
                     <div className="row">
                         <div className="col">
                             <h1 className="main-title">Il mio blog</h1>
+                        </div>
+                        <div className="col">
                             <div className="tags-container">
                                 {tags.map((tag, i) => {
                                     return <span key={i}>{tag}</span>;
                                 })}
                             </div>
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type="text"
+                                    value={postName}
+                                    onChange={(e) => {
+                                        setPostName(e.target.value);
+                                    }}
+                                />
+                                <button>Invia</button>
+                            </form>
                         </div>
-                        {posts.map((post) => {
+                        {drawnPosts.map((post) => {
                             return (
                                 post.published && (
                                     <div className="col-6">
